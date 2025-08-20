af76d933f5596fe5ef5ffc8d6a9dff3dc77346a4
nicevall <160814347+nicevall@users.noreply.github.com>
2025-08-20 06:40:09 -0500

    v6

--- src/routes/locationRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/locationRoutes.js
-const express = require('express');
-const router = express.Router();
-const locationController = require('../controllers/location.Controller');
-
-// POST /api/location/update
-router.post('/update', locationController.updateUserLocation);
-
-module.exports = router;
